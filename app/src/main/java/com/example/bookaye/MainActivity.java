2a1a5f777cfa2dd21e30dc58e8ba87f0e2309350
Lucas <segawa.l@husky.neu.edu>
2019-11-16 14:52:35 -0500

    Image retrieval

--- app/src/main/java/com/example/bookaye/MainActivity.java
+++ app/src/main/java/com/example/bookaye/MainActivity.java
@@ -3,19 +3,44 @@ package com.example.bookaye;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.hardware.Camera;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.android.material.textfield.TextInputEditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.FrameLayout;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    static final int REQUEST_IMAGE_CAPTURE = 1;
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == RESULT_OK) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            this.image.setImageBitmap(imageBitmap);
+        }
+    }
+
+    private ImageView image;
+    private TextInputEditText text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,12 +49,12 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         FloatingActionButton fab = findViewById(R.id.fab);
-        // Create an instance of Camera
+        text = findViewById(R.id.text);
+        image = findViewById(R.id.image);
         fab.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                startActivity(new Intent(MainActivity.this, CameraActivity.class));
+                dispatchTakePictureIntent();
             }
         });
-
     }
 }
